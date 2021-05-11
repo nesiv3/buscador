f33ac57f2f92c96a366fb4891d28fb4356dcf79e
Néstor Iván Vargas Muñoz <nvargas@asesoftware.com>
2021-05-10 22:07:51 -0500

    Ajuste busqueda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 import { TipoBusqueda } from 'src/app/objetos/TipoBusqueda';
-import { Paginas, PaginasEspacializadosEspañol,  PaginasEspañol } from './objetos/Paginas';
+import { Paginas, PaginasEspacializadosEspañol, PaginasEspañol } from './objetos/Paginas';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,55 +13,52 @@ import { Paginas, PaginasEspacializadosEspañol,  PaginasEspañol } from './obje
 })
 export class AppComponent {
   title = 'buscador';
-  srcUrl= 'https://dle.rae.es/';
+  srcUrl = 'https://dle.rae.es/';
   srcTranslate: "https://www.google.com"
   fecha = Date.now().toLocaleString();
   favoriteSeason: string;
   paginasBusqueda: Paginas[] = [];
-  textoABuscar : String = "";
+  seleccion: TipoBusqueda = new TipoBusqueda();
+  textoABuscar: String = "";
   busqueda: TipoBusqueda[] = [
     {
       Id: 1,
-      Descripcion: 'Diccionarios generales en español',
-      Checked:false,
+      Descripcion: 'ES-FR',
+      Checked: false,
       Paginas: PaginasEspañol
     },
     {
       Id: 2,
-      Descripcion: 'Diccionarios especializados en español',
-      Checked:false,
-      Paginas:PaginasEspacializadosEspañol
+      Descripcion: 'ES',
+      Checked: false,
+      Paginas: PaginasEspacializadosEspañol
     },
-    { Id: 3, Descripcion: 'Diccionarios generales en frances', Checked:false,   Paginas:PaginasEspacializadosEspañol },
-    { Id: 4, Descripcion: 'Diccionarios especializados en inglés', Checked:false ,   Paginas:PaginasEspacializadosEspañol},
-    { Id: 5, Descripcion: 'Traducción del inglés al español', Checked:false ,   Paginas:PaginasEspacializadosEspañol},
-    { Id: 6, Descripcion: 'Traducción especializada del inglés al español', Checked:false,   Paginas:PaginasEspacializadosEspañol },
-    { Id: 7, Descripcion: 'Traducción del francés al español' , Checked:false,   Paginas:PaginasEspacializadosEspañol},
-    { Id: 8, Descripcion: 'Diccionarios de catalán', Checked:false ,   Paginas:PaginasEspacializadosEspañol}
+    { Id: 3, Descripcion: 'FR', Checked: false, Paginas: PaginasEspacializadosEspañol },
 
   ];
 
   resultado: string[] = []
 
-  myFunction()
-  {
-    let paginas : Paginas[] = this.busqueda.find(m=>m.Checked === true).Paginas;
+
+
+  myFunction() {
+    let paginas: Paginas[] = this.seleccion.Paginas;
     this.paginasBusqueda = paginas.slice();
 
- }
- evento(data:Paginas)
- {
-  
-   if(data.Reemplazo)
-   {
-    this.srcUrl = data.URL.replace('=brucomodin','='+this.textoABuscar.toString());
-   }
-   else{
-    this.srcUrl =  data.URL+this.textoABuscar;
+
   }
- //
- 
+  evento(data: Paginas) {
 
+    if (data.Reemplazo) {
+      this.srcUrl = data.URL.replace('=brucomodin', '=' + this.textoABuscar.toString());
+    }
+    else {
+      this.srcUrl = data.URL + this.textoABuscar;
+    }
+    //
+  }
+  systemChange(data: TipoBusqueda) {
+    this.seleccion = data;
+  }
 
- }
 }
--- src/app/objetos/Paginas.ts
+++ src/app/objetos/Paginas.ts
@@ -2,20 +2,92 @@ export class  Paginas
 {
 
 URL: string;
-    Descripcion:string;
-
+    Nombre:string;
+Puntuacion:number;
    Reemplazo:boolean= false;
-
+Descripcion:string;
 
 }
 
 export const PaginasEspañol: Paginas[] = [
 
-    {URL:"https://dle.rae.es/", Descripcion:"RAE",Reemplazo:false },
-    {URL:"https://en.wikipedia.org/wiki/", Descripcion:"Wikipedia",Reemplazo:false },
+    {URL:'https://www.hetop.eu/hetop/es/?q= ',Nombre:'HeTOP',Puntuacion:50,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://iate.europa.eu/search/standard/ ',Nombre:'IATE',Puntuacion:43,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://icd.who.int/browse11/l-m/es#/',Nombre:'Denominaciones comunes internacionales (DCI) ',Puntuacion:36,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://artes.app.univ-paris-diderot.fr/ ',Nombre:'ARTES',Puntuacion:31,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.traducmed.fr/ ',Nombre:'Traducmed',Puntuacion:0,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.btb.termiumplus.gc.ca/tpv2alpha/alpha-fra.html?lang=fra&i=1&srchtxt= ',Nombre:'Termium',Puntuacion:47,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.termsciences.fr/ ',Nombre:'Termsciences',Puntuacion:38,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://traduction.sensagent.com/traduire/fr-es/',Nombre:'Sensagent',Puntuacion:37,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.wordreference.com/fres//',Nombre:'WordReference',Puntuacion:33,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.linguee.es/espanol-frances/',Nombre:'Linguee',Puntuacion:32,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://diccionario.reverso.net/frances-espanol/',Nombre:'Reverso',Puntuacion:30,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://umd.emro.who.int/whodictionary/',Nombre:'WHO Dictionary',Puntuacion:25,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.proz.com/search/',Nombre:'Proz.com',Puntuacion:22,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://glosario.sld.cu/multilingue/fr/ ',Nombre:'Glosario Multilingüe de Términos Médicos Infomed',Puntuacion:21,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://dictionnaire.acadpharm.org/w/Acadpharm:Accueil',Nombre:'Dictionnaire de l’Académie Nationale de Pharmacie',Puntuacion:50,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.vidal.fr/recherche.html?query= ',Nombre:'Vidal',Puntuacion:42,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://dictionnaire.academie-medecine.fr/index.php ',Nombre:'Dictionnaire médicale de l’Académie de Médecine',Puntuacion:42,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://gdt.oqlf.gouv.qc.ca/ ',Nombre:'Grand dictionnaire terminologique',Puntuacion:51,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.cnrtl.fr/definition/ ',Nombre:'CNRTL',Puntuacion:34,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.bio-top.net/Terminologie/Menu/terminologie__medicale_menu.htm ',Nombre:'LEXIQUE  DE  TERMINOLOGIE  MÉDICALE',Puntuacion:28,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.doctissimo.fr/html/sante/encyclopedie/encyclopedie_medicale.htm ',Nombre:'Encyclopédie médicale Doctissimo',Puntuacion:28,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.medisite.fr/dictionnaire-des-maladies/5',Nombre:'Dictionnaires Medisite',Puntuacion:27,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.esculape.com/cherche/dico_medical.html#628 ',Nombre:'Esculape - Petit lexique de termes médicaux',Puntuacion:22,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.vulgaris-medical.com/encyclopedie-medicale ',Nombre:'Vulgaris médical',Puntuacion:20,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.myonet.org/GENETIQUE/glossaire.html ',Nombre:'GLOSSAIRE de GENETIQUE MOLECULAIRE - GENIE GENETIQUE',Puntuacion:18,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://dtme.ranm.es/index.aspx ',Nombre:'Diccionario de términos médicos de la RANM',Puntuacion:52,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://decs2020.bvsalud.org/cgi-bin/wxis1660.exe/decsserver/?IsisScript=../cgi-bin/decsserver/decsserver.xis&interface_language=e&previous_page=homepage&previous_task=NULL&task=start ',Nombre:'DeCS',Puntuacion:44,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://dicciomed.usal.es/palabra/ ',Nombre:'Dicciomed',Puntuacion:44,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://enclavedeciencia.rae.es/',Nombre:'Enclave de ciencia',Puntuacion:42,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.portalesmedicos.com/diccionario_medico/index.php?title= ',Nombre:'Medicopedia',Puntuacion:41,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://dle.rae.es/',Nombre:'Diccionario RAE',Puntuacion:40,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.cun.es/diccionario-medico ',Nombre:'Diccionario médico CUN',Puntuacion:37,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.iqb.es/diccio/diccio1.htm',Nombre:'Medciclopedia IQB',Puntuacion:34,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.diccionariomedico.net/diccionario-terminos ',Nombre:'Diccionario médico ',Puntuacion:28,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.laenfermeria.es/docuwiki/doku.php?id=siglas_medicas ',Nombre:'Diccionario laenfermeria – Siglas médicas',Puntuacion:26,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.ugr.es/~oncoterm/alpha-index.html ',Nombre:'OncoTerm',Puntuacion:25,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.laenfermeria.es/docuwiki/doku.php?id=start&do=backlink ',Nombre:'Diccionario laenfermeria WIKI',Puntuacion:24,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.aecc.es/es/todo-sobre-cancer/glosario ',Nombre:'Glosario AECC',Puntuacion:19,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.asefasalud.es/informacion/diccionario-especialidades ',Nombre:'Diccionario de especialidades Asefa Salud',Puntuacion:17,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.asefasalud.es/informacion/diccionario-de-terminos-medicos ',Nombre:'Diccionario de términos médicos Asefa Salud',Puntuacion:17,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'http://www.wikilengua.org/index.php/Glosario_de_medicina ',Nombre:'Glosario de medicina – Wikilengua del español',Puntuacion:17,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.acta.es/recursos/glosarios-especializados/glosarios-trilinguees',Nombre:'Glosarios trilingües ACTA',Puntuacion:23,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.acta.es/recursos/glosarios-especializados/listados-de-glosarios?orderby=Grupo&ordering=ASC ',Nombre:'Glosarios de especialidades ACTA',Puntuacion:13,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.acta.es/recursos/glosarios-especializados/listados-de-glosarios?orderby=Grupo&ordering=ASC ',Nombre:'Glosarios de especialidades ACTA',Puntuacion:13,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:13,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:12,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:12,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:11,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:11,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:11,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:11,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:10,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Glosarios de especialidades ACTA',Puntuacion:10,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.tremedica.org/tremediteca/glosarios/ ',Nombre:'CDI medicamentos',Puntuacion:12,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Diccionario de términos normalizados de formas farmacéuticas sólidas',Puntuacion:9,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://dialnet.unirioja.es/servlet/articulo?codigo=4660330 ',Nombre:'Glosario terminológico español-francés sobre ENM pediátricas',Puntuacion:20,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://ebuah.uah.es/dspace/handle/10017/46489',Nombre:'Glosario sobre cánceres ginecológicos',Puntuacion:18,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://crbm.chu-rouen.fr/querybuilder/# ',Nombre:'CRBM',Puntuacion:49,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.ameli.fr/assure/recherche?text= ',Nombre:'Ameli',Puntuacion:41,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://smt.esante.gouv.fr/explorer-les-concepts/?lang=fr ',Nombre:'SMT - Agence du numérique en santé',Puntuacion:36,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.hon.ch/HONselect/index_f.html',Nombre:'HONSelect',Puntuacion:36,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.lissa.fr/dc/#env=lissa&q= ',Nombre:'Lissa',Puntuacion:40,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'Doocteur',Puntuacion:32,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://doccismef.chu-rouen.fr/dc/#env=basic ',Nombre:'Doc’CISMeF',Puntuacion:36,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.myobase.org/ ',Nombre:'Portail documentaire sur les maladies neuromusculaires',Puntuacion:27,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.urofrance.org/search/node/ ',Nombre:'Association Française durologie',Puntuacion:27,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.elcomprimido.com/es/?option=com_googlesearch_cse&n=30&view=googlesearchs&Itemid=&cx=008151878029964939678%3Al2v68yhupyq&cof=FORID%3A11&ie=UTF-8&q=',Nombre:'elComprimido',Puntuacion:33,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.vademecum.es/medicamentos-a_1   ',Nombre:'Vademécum',Puntuacion:33,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://medes.com/Public/Home.aspx ',Nombre:'MEDES',Puntuacion:31,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.aecc.es/es/buscador-avanzado/global?search_api_fulltext= ',Nombre:'Buscador AECC',Puntuacion:11,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://fr.pons.com/traduction ',Nombre:'Pons',Puntuacion:25,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://www.deepl.com/translator#fr/es/',Nombre:'DeepL',Puntuacion:18,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'https://translate.google.com/',Nombre:'Google Translate',Puntuacion:18,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'MedLinePlus',Puntuacion:14,Reemplazo:false,Descripcion:'Descripción de la página '},
+{URL:'',Nombre:'HAS Santé',Puntuacion:37,Reemplazo:false,Descripcion:'Descripción de la página '},
 
-    {URL:"http://recursosdidacticos.es/goodrae/index.php?x=0&y=0&_pagi_pg=1&buscar=", Descripcion:"recursosdidacticos",Reemplazo:false },
-    {URL:"http://www.termsciences.fr/services/urls/recherche/?qry=brucomodin&lng=fr", Descripcion:"termsciences",Reemplazo:true },
+    
 
 ];
 
